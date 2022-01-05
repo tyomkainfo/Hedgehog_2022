b91f7baf37a59fab5e9d767608fd9e0b50b2cb13
tyomka.info <tyomka.info@gmail.com>
2022-01-05 15:05:42 +0300

    new tamplate

---
+++ dist/scripts/script.js
@@ -0,0 +1,44 @@
+function burgerMenu(icon) {
+    icon.classList.toggle("change");
+}
+$(document).ready(function () {
+    $('.owl-carousel_1').owlCarousel({
+        loop: true,
+        margin: 24,
+        nav: true,
+        navText: ['', ' '],
+
+        responsive: {
+            0: {
+                items: 1.2
+            },
+
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+    if ($(window).width() < 992) {
+        $('.owl-carousel_2').owlCarousel({
+            loop: true,
+            margin: 24,
+            nav: true,
+            navText: ['', ' '],
+
+            responsive: {
+                0: {
+                    items: 1.1
+                },
+
+                1000: {
+                    items: 3
+                }
+            }
+        })
+    } else {
+        $('.owl-carousel_2').owlCarousel('destroy')
+    }
+});
+
+
